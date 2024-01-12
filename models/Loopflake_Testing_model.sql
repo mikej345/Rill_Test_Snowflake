247b92b2e9049d094a5183a11c73e150645949bf
mikej345 <mpj567@gmail.com>
2024-01-12 09:45:33 -0800

    Update Loopflake_Testing_model.sql

--- models/Loopflake_Testing_model.sql
+++ models/Loopflake_Testing_model.sql
@@ -1,1 +1,3 @@
-select * from Loopflake_Testing
+select *, coalesce(eam.PRIORITY,'Non-Enterprise') as Enterprise_Importance, 
+from Loopflake_Testing lft
+left outer join enterprise_analysis_map eam on eam.business_id = lft.business_id
